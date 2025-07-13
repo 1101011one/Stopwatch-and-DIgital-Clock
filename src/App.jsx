4dca825cfb1de32abf2f1b7f291ef6bf49d1823f
1101011one <1101011one@gmail.com>
2025-07-13 09:57:52 +0530

    added files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import DigitalClock from './DigitalClock.jsx';
+import StopWatch from './StopWatch.jsx';
+
+function App() {
+      
+  return(
+    <>
+    <StopWatch/>
+    <DigitalClock/>
+    </>
+  );
+  
+}
+
+export default App
